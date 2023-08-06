474e427fec944f683e89d62aa6430d682cffbb25
Javen Wilson <javenb022@gmail.com>
2023-08-05 22:28:41 -0400

    Sad path: random country

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    recipes = RecipeFacade.new.get_recipes(params[:q])
-    render json: RecipeSerializer.new(recipes)
+    if params[:q]
+      recipes = RecipeFacade.new.get_recipes(params[:q])
+      render json: RecipeSerializer.new(recipes)
+    else
+      country = CountryFacade.random_country
+      recipes = RecipeFacade.new.get_recipes(country)
+      render json: RecipeSerializer.new(recipes)
+    end
   end
 end
