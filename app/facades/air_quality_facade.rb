982b1347d3ab0061c2335c06f814ec0a6305b09d
Javen Wilson <javenb022@gmail.com>
2023-08-07 11:54:25 -0400

    Unnecessary file

--- app/facades/air_quality_facade.rb
+++
@@ -1,7 +0,0 @@
-class AirQualityFacade
-  def self.get_air_quality_data(country)
-    city = CountryService.new.get_country(country)[0][:capital][0]
-    data = AirQualityService.new.get_air_quality_data(city)
-    # AirQuality.new(data)
-  end
-end
