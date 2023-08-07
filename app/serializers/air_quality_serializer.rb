7e51d3375343d126f8c22868ef4ce6068a1adab6
Javen Wilson <javenb022@gmail.com>
2023-08-07 11:58:26 -0400

    Feat: json formatting

--- app/serializers/air_quality_serializer.rb
+++ app/serializers/air_quality_serializer.rb
@@ -1,2 +1,16 @@
 class AirQualitySerializer
+  def air_quality_response(data, city)
+    {
+      data: {
+        id: nil,
+        type: "air_quality",
+        city: city,
+        attributes: {
+          aqi: data[:overall_aqi],
+          pm25_concentration: data[:"PM2.5"][:concentration],
+          co_concentration: data[:CO][:concentration]
+        }
+      }
+    }
+  end
 end
