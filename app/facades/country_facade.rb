761d5be4ef82ca361139f3e95b6971a003aef2b1
Javen Wilson <javenb022@gmail.com>
2023-08-07 11:54:42 -0400

    Feat: helper method to retrieve capital of country

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -4,4 +4,8 @@ class CountryFacade
     num = rand(1..country.count)
     country[num][:name][:common]
   end
+
+  def get_city(country)
+    CountryService.new.get_country(country)[0][:capital][0]
+  end
 end
