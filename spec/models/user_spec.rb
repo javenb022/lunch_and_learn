013b86c6bf720a98a0d7c2d040feb4614b6f62c8
Javen Wilson <javenb022@gmail.com>
2023-08-07 23:52:52 -0400

    Test: user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe User do
     it { should validate_uniqueness_of :email }
     it { should have_secure_password }
     it { should validate_presence_of :password }
+    it { should have_many :api_keys }
+    it { should have_many :favorites }
   end
 end
